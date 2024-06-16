de605c27f64cf46aee4423abf90d1cfc673f978b
wisdomska <wisdomska@gmail.com>
2024-06-16 16:36:26 +0000

    Abstract

---
+++ Abstract/Program.cs
@@ -0,0 +1,17 @@
+﻿using Abstract;
+using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(7.0);
+            Shape myRectangle = new Rectangle(5.0, 12.0);
+
+            Console.WriteLine($"Area of Circle: {myCircle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {myRectangle.GetArea()}");
+        }
+    }
+}
